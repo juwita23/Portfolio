36802f0bfdd2a3779f55ff992cc17a17fb3633e8
juwitaanr <119846701+juwita23@users.noreply.github.com>
2026-09-02 09:58:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+// Force scroll to top on refresh
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0);
+
 const phrases = [
     "Data Analyst",
     "Problem Solver",
